2be10e529c60040fdf36774681cc3d66da043c2c
ClaudeClaw <health-agent@claudeclaw>
2026-04-28 17:50:07 +0300

    Fix: add kotlin-parcelize plugin required by Samsung Health Data SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-parcelize") // Required by Samsung Health Data SDK internal Parcelable usage
 }
 
 android {
